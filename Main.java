c3cd18f43c4fe131419c03feb137b86de4f20b76
Артем <marcus201@mail.ru>
2022-12-04 00:51:50 +0500

    фибоначчи

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextInt();
+        long[] fibonachi = new long[(int) n];
+        fibonachi [0] = 0;
+        fibonachi [1] = 1;
+        for (int i = 2; i < fibonachi.length; ++i) {
+            fibonachi[i] = fibonachi[i - 1] + fibonachi[i - 2];
+            System.out.println(fibonachi[i]);
+        }
     }
 }
